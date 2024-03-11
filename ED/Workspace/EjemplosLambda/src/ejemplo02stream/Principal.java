745a953a8246f980034532873f65aadac0a0db24
Pilar <aguilar.dimar23@triana.salesianos.edu>
2024-03-11 13:31:11 +0100

    11 marzo 13.30

---
+++ ED/Workspace/EjemplosLambda/src/ejemplo02stream/Principal.java
@@ -0,0 +1,18 @@
+package ejemplo02stream;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Para usar un Stream siempre se tiene primero la lista 
+		// de los que sea y luego "." y se llama al stream: "lista.stream()..."
+
+		// EJ: metodoX{return CrudCoche.buscarVendidos().stream().count();}
+		// listaCoche.stream()
+		//	.filter(coche->coche.getModelo().equalsIgnoreCase.limit(limit).list()); Esto es continuación de la líne ade arriba
+		
+		
+	}
+
+}
---
+++ LMSGI/Ejercicios/Tema8/ejemplos/Recursos/ejemplo01.js
@@ -0,0 +1,12 @@
+let mes = prompt("Indica un mes");  //para definir variable siempre let. el prompt para pedir por teclado. las variables no se le pone el tipo de dato que es
+alert("Estamos en " + mes);
+
+
+
+
+alert("Hola mundo"); // el alert es una ventana emergente al modo consola. de momento nos funciona como un syso
+
+// en los arrays se pueden mezclar distintos datos: string,números...
+
+// === ese triple igual significa comprobar que el valor y el tipo de datos son iguales  , 
+// es decir ('12'=== 12)= false; comprueba que el 12 es un string o integer y si es el mismo valor. como no es igual pone falso
