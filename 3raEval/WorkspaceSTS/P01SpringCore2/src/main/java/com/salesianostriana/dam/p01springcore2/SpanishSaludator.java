5dd676a265150914eaa65dc300c12f2909b3a119
Pilar <aguilar.dimar23@triana.salesianos.edu>
2024-04-22 14:29:40 +0200

    22 abr 14.30

--- 3raEval/WorkspaceSTS/P01SpringCore2/src/main/java/com/salesianostriana/dam/p01springcore2/SpanishSaludator.java
+++ 3raEval/WorkspaceSTS/P01SpringCore2/src/main/java/com/salesianostriana/dam/p01springcore2/SpanishSaludator.java
@@ -3,11 +3,23 @@ package com.salesianostriana.dam.p01springcore2;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class SpanishSaludator implements Saludator{
 
 	@Override
 	public void imprimirSaludo() {
 		System.out.println("Hola a 1DAM");
 	}
+	
+	@PostConstruct
+	void alComenzar(){
+		System.out.println("Método \"alComenzar\" llamado");
+	}
 
+	@PreDestroy
+	void antesDeDestruir() {
+		System.out.println("Método \"antesDeDestruir\" qu se ejecuta antes de que se destruya.");
+	}
 }
--- 3raEval/WorkspaceSTS/Prueba01TiendaOnline/src/main/java/com/salesianostriana/dam/prueba01/Product.java
+++ 3raEval/WorkspaceSTS/Prueba01TiendaOnline/src/main/java/com/salesianostriana/dam/prueba01/Product.java
@@ -1,8 +1,10 @@
 package com.salesianostriana.dam.prueba01;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
+@Data @AllArgsConstructor @NoArgsConstructor
 public class Product {
 
 	private int productId;
