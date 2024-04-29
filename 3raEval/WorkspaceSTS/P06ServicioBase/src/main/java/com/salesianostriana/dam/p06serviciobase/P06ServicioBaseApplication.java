88fc5d0dc1a8c78261b38dc871fa71f9cc3fdbed
Pilar <aguilar.dimar23@triana.salesianos.edu>
2024-04-29 14:31:03 +0200

    29 abr 14.30 repositorio nuevo creado - organizar proyecto en fragmentos

---
+++ 3raEval/WorkspaceSTS/P06ServicioBase/src/main/java/com/salesianostriana/dam/p06serviciobase/P06ServicioBaseApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.p06serviciobase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P06ServicioBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P06ServicioBaseApplication.class, args);
+	}
+
+}
---
+++ 3raEval/WorkspaceSTS/P06ServicioBase/src/test/java/com/salesianostriana/dam/p06serviciobase/P06ServicioBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.p06serviciobase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P06ServicioBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
